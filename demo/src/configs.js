f8d49eed0f2fd9cb249887126a6d463a2b02a298
MorganDream <dreameng023@gmail.com>
2020-03-23 17:50:43 +0800

    more refactor

---
+++ demo/src/configs.js
@@ -0,0 +1,14 @@
+"use strict";
+// Copyright (c) 2019 Swisscom Blockchain AG
+// Licensed under MIT License
+exports.__esModule = true;
+var seraph_id_sdk_1 = require("@sbc/seraph-id-sdk");
+exports.NEO_RPC_URL = 'https://demo.seraphid.io/rpc';
+exports.NEOSCAN_URL = 'https://demo.seraphid.io/api';
+exports.DID_NETWORK = seraph_id_sdk_1.DIDNetwork.PrivateNet;
+exports.GOVERNMENT_SCRIPT_HASH = 'b4eeeb80d20bfb38b23001d0659ce0c1d96be0aa';
+exports.AGENCY_SCRIPT_HASH = 'a576ca22bb406a8597f8b0d5e62e52ced4d0bbdf';
+exports.GOVERNMENT_ISSUER_PRIVATE_KEY = '66b8bcf4202da680101bbb53aecb077afcdd4cf98af5d716ddf4a629c0a7d30a';
+exports.AGENCY_ISSUER_PRIVATE_KEY = 'a66eba251d3a28c57b0656d4d2efc9e262c87a810b3c7afdd31636ccfed4afa1';
+exports.PASSPORT_SCHEMA_NAME = 'Passport';
+exports.ACCESS_KEY_SCHEMA_NAME = 'AccessKey';
